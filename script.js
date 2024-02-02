f09ae27288c5626e216076151092065146d857cd
kishorgithub123 <142067255+kishorgithub123@users.noreply.github.com>
2024-02-02 21:15:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+gsap.from("#main",{
+    x:10,
+    y:-20,
+    opacity:1,
+    duration:0.5,
+    color:"yellow",
+    delay:0.5,
+    stagger:1
+})
+gsap.to("#first",{
+    x:-250,
+    y:-140,
+    opacity:1,
+    duration:1,
+    color:"red",
+    delay:1.5,
+    stagger:1
+})
+gsap.to("#second",{
+    x:-150,
+    y:-150,
+    opacity:1,
+    duration:1,
+    color:"aqua",
+    delay:2.5,
+    stagger:1
+})
+gsap.to("#third",{
+    x:-100,
+   y:-150,
+   opacity:1,
+   duration:1,
+   color:"salmon",
+   delay:3.5,
+   stagger:1
+})
+gsap.to("#fourth",{
+
+   y:-150,
+   opacity:1,
+   duration:1,
+   color:"purple",
+   delay:3.5,
+   stagger:1
+})
+gsap.to("#magic",{
+    x:300,
+    y:150,
+    opacity:1,
+    duration:1,
+    color:"pink",
+    delay:4,
+    stagger:1
+ })
+ gsap.to("#magic2",{
+    x:-200,
+    y:150,
+    opacity:1,
+    duration:1,
+    color:"blue",
+    delay:5,
+    stagger:1
+ })
+ gsap.to("#img2",{
+    x:-200,
+    y:150,
+    opacity:1,
+    scale:0.4,
+    duration:1,
+    color:"white",
+    delay:6,
+    stagger:1
+ })
+ gsap.to("#img3",{
+    x:200,
+    y:-100,
+    opacity:1,
+    scale:0.7,
+    duration:1,
+    color:"purple",
+    delay:7,
+    stagger:1
+ })
+ gsap.to("#img4",{
+    x:-200,
+    y:-10,
+    opacity:1,
+    scale:0.5,
+    duration:1,
+    color:"blue",
+    delay:9,
+    stagger:1
+ })
+
+
+
+
+// let timeline=gsap.timeline()
+// timeline.to("#first",{
+//    color:"grey"
+// })
+// gsap.to("#nav2",{
+//     y:20,
+//     opacity:1,
+//     duration:1,
+//     stagger:1
+// })
+
